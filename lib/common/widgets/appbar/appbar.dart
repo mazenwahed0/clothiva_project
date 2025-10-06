a5a4a5ba1ae1b8833baa6d5e6dc8273841489df1
3li7assan <146840446+Ali22017684@users.noreply.github.com>
2025-10-06 15:50:30 +0300

    Editing(circular_container)

--- lib/common/widgets/appbar/appbar.dart
+++ lib/common/widgets/appbar/appbar.dart
@@ -2,12 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
+import '../../../utils/constants/colors.dart';
+import '../../../utils/device/device_utility.dart';
+import '../../../utils/helpers/helper_functions.dart';
+
 class CAppBar extends StatelessWidget implements PreferredSizeWidget {
+  /// Custom appbar for achieving a desired design goal.
+  /// - Set [title] for a custom title.
+  /// - [showBackArrow] to toggle the visibility of the back arrow.
+  /// - [leadingIcon] for a custom leading icon.
+  /// - [leadingOnPressed] callback for the leading icon press event.
+  /// - [actions] for adding a list of action widgets.
+  /// - Horizontal padding of the appbar can be customized inside this widget.
   const CAppBar({
     super.key,
     this.title,
+    this.actions,
     this.leadingIcon,
     this.leadingOnPressed,
+    this.showBackArrow = false,
   });
 
   final Widget? title;
@@ -18,14 +31,16 @@ class CAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      child: AppBar(
-        automaticallyImplyLeading: false,
-        leading: showBackArrow
-            : leadingIcon != null
-            : null,
-        title: title,
-      ),
+    final dark = CHelperFunctions.isDarkMode(context);
+    return AppBar(
+      automaticallyImplyLeading: false,
+      leading: showBackArrow
+          ? IconButton(onPressed: () => Get.back(), icon: Icon(Iconsax.arrow_left, color: dark ? CColors.white : CColors.dark))
+          : leadingIcon != null
+          ? IconButton(onPressed: leadingOnPressed, icon: Icon(leadingIcon))
+          : null,
+      title: title,
+      actions: actions,
     );
   }
 
